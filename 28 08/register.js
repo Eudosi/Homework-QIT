7e0966556152cfb683bf08255ffe7fa466da969e
EldosB <eldosbejsenbaj2@gmail.com>
2024-10-30 10:14:54 +0500

    add js

---
+++ 28 08/register.js
@@ -0,0 +1,15 @@
+var name = "N"; // Ауыспалы айнымалы
+
+name = "A";
+
+name ="S";
+
+name = "A";
+
+const phone ="192/168.123.10"; //Тұрақты
+
+phone = "192.172.35.10";
+
+console.log(phone); // Қате қайтарады
+
+document/getElementById("username").innerHTML = name
