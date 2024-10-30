b41007e94ed6b183e4ba427e812e963bb8e2a466
EldosB <eldosbejsenbaj2@gmail.com>
2024-10-30 10:43:22 +0500

    add  js

--- 28 08/register.js
+++ 28 08/register.js
@@ -13,3 +13,17 @@ phone = "192.172.35.10";
 console.log(phone); // Қате қайтарады
 
 document/getElementById("username").innerHTML = name
+
+
+var streetName = "Maulenov, 92"; // String
+var currentDay =30; // Boolean true = шындықб false = өтірік
+var AlimKeldi = false; // Бос мән
+var JanaSumka = null; //Бос мән
+var porshe = undefined; //Жоқ айнымалы
+
+
+console.log("Көше", streeName);
+console.log("Қазіргі күне", currentDay);
+console.log("Әлім келді!", AkimKeldi);
+console.log("Жаңа сатып алған сумакның ішіндегі заттар:", JanaSumka);
+console.log("Порше бар", porshe)
